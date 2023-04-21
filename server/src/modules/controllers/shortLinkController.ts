ce9104f7caee59c6eef3c94f8c4ee8268f1ae367
Ugochukwu Amajor <maximmajor4us@gmail.com>
2023-04-21 15:31:57 +0100

    modification of the encode, decode and stats endpoint

--- server/src/modules/controllers/shortLinkController.ts
+++ server/src/modules/controllers/shortLinkController.ts
@@ -5,54 +5,44 @@ class shortLinkController {
   public shortLinkRepository = new shortLinkRepository();
 
   public async encode(req: Request, res: Response): Promise<void> {
-
     try {
       const { originalUrl } = req.body;
-
       if (!originalUrl) {
         res.status(400).json({ error: 'Original URL is required' });
         return;
       }
-
       const existingUrl = await this.shortLinkRepository.findOneByOriginalUrl(originalUrl);
-
       const baseUrl = `${req.protocol}://${req.get('host')}/`;
       if (existingUrl) {
-
         res.json({ shortUrl: existingUrl.shortUrl });
         return;
       }
-
       const shortLink = await this.shortLinkRepository.createShortUrl(originalUrl, baseUrl);
-
       res.json({ shortUrl: shortLink.shortUrl });
     } catch (err) {
       res.status(500).json({ error: 'Internal server error' });
     }
   }
 
+
   public async decode(req: Request, res: Response): Promise<void> {
     try {
       const { shortUrl } = req.body;
-
       if (!shortUrl) {
         res.status(400).json({ error: 'Short URL is required' });
         return;
       }
-
       const shortLink = await this.shortLinkRepository.findOneByShortUrl(shortUrl);
-
-
       if (!shortLink) {
         res.status(404).json({ error: 'Short URL not found' });
         return;
       }
-
       res.json({ originalUrl: shortLink.originalUrl });
     } catch (err) {
       res.status(500).json({ error: 'Internal server error' });
     }
   }
+
 
   public async statistic(req: Request, res: Response): Promise<void> {
     try {
@@ -61,9 +51,7 @@ class shortLinkController {
         res.status(400).json({ error: 'URL path is required' });
         return
       }
-
-      const urlStat = await this.shortLinkRepository.findOneByShortUrl(urlPath);
-
+      const urlStat = await this.shortLinkRepository.shortUrlPart(urlPath);
       if (!urlStat) {
         res.status(404).json({ error: 'Short URL not found' });
         return
@@ -84,8 +72,7 @@ class shortLinkController {
         res.status(400).json({ error: 'URL path is required' });
         return
       }
-
-      const urlStat = await this.shortLinkRepository.findOneByShortUrl(urlPath);
+      const urlStat = await this.shortLinkRepository.shortUrlPart(urlPath);
 
       if (!urlStat) {
         res.status(404).json({ error: 'Short URL not found' });
@@ -96,7 +83,6 @@ class shortLinkController {
       res.status(500).json({ error: 'Internal server error' });
     }
   }
-
 }
 
 export default shortLinkController;
--- server/src/modules/repositories/shortLinkRepository.ts
+++ server/src/modules/repositories/shortLinkRepository.ts
@@ -9,22 +9,27 @@ class ShortLinkRepository {
   public async findOneByOriginalUrl(originalUrl: string): Promise<IShortLink | null> {
     const findOriginalUrl = await this.shortLinkModel.findOne({ originalUrl });
     return findOriginalUrl
-
   }
 
-  public async findOneByShortUrl(ShortUrlPart: string): Promise<IShortLink | null> {
-    return await this.shortLinkModel.findOne({ ShortUrlPart });
+
+  public async shortUrlPart(ShortUrlPart: string): Promise<IShortLink | null> {
+    const shortUrlPart = await this.shortLinkModel.findOne({ ShortUrlPart });
+    return shortUrlPart
   }
 
-  public async createShortUrl(originalUrl: string, baseUrl: string): Promise<IShortLink> {
-    
+
+  public async findOneByShortUrl(shortUrl: string): Promise<IShortLink | null> {
+   const findShortUrl = await this.shortLinkModel.findOne({ shortUrl });
+   return findShortUrl
+  }
+
+  public async createShortUrl(originalUrl: string, baseUrl: string): Promise<IShortLink> {  
     const generateShortUrlPath = `${shortid.generate()}`
     const newShortLink = {
       originalUrl: originalUrl,
       shortUrl: `${baseUrl}${generateShortUrlPath}`,
       ShortUrlPart: generateShortUrlPath
     };
-
     const createShortUrl = await this.shortLinkModel.create(newShortLink);
     return createShortUrl
   }
